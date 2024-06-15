3d6fc0c09769ef7a3b4c1f181e88a3c00e425f34
Mahkamjon Raupov <devbrovki@gmail.com>
2024-06-16 01:10:04 +0200

    MessageController v3 and MessageDTO added

--- src/main/java/com/example/chatsystem/controller/MessageController.java
+++ src/main/java/com/example/chatsystem/controller/MessageController.java
@@ -1,52 +1,46 @@
 package com.example.chatsystem.controller;
 
-import com.example.chatsystem.model.Message;
+import com.example.chatsystem.dto.MessageDTO;
+import com.example.chatsystem.security.MyUserDetails;
+import com.example.chatsystem.service.ChatService;
 import com.example.chatsystem.service.MessageService;
-import com.example.chatsystem.service.UserService;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/messages")
+@RequestMapping("/api/v2/messages")
 public class MessageController {
-    private MessageService messageService;
+    private final MessageService messageService;
+    private final ChatService chatService;
 
     @Autowired
-    public MessageController(MessageService messageService) {
-
+    public MessageController(MessageService messageService, ChatService chatService) {
         this.messageService = messageService;
+        this.chatService = chatService;
     }
 
-    @GetMapping
-    public List<Message> findAllMessages(@RequestParam("collection") String collectionName) {
-        return messageService.findAllMessages(collectionName);
+
+    @GetMapping("/")
+    public ResponseEntity<List<MessageDTO>> getAllMessages(@AuthenticationPrincipal MyUserDetails user) {
+        List<MessageDTO> messageDTOS = new ArrayList<>();
+        return ResponseEntity.ok(messageDTOS);
     }
 
-    @GetMapping("/{id}")
-    public Message findMessageById(@RequestParam("collection") String collectionName, @PathVariable String id) {
-        return messageService.findMessageById(collectionName, id);
+    @GetMapping("/{username}")
+    public ResponseEntity<List<MessageDTO>> getMessages(@AuthenticationPrincipal MyUserDetails userDetails, @PathVariable String username){
+        List<MessageDTO> messageDTOS = messageService.getChatMessages(userDetails, username);
+        return ResponseEntity.ok(messageDTOS);
     }
 
-    @PostMapping
-    public Message saveMessage(@RequestParam("collection") String collectionName, @RequestBody Message message) {
-        return messageService.saveMessage(collectionName, message);
-    }
-
-    @PutMapping("/{id}")
-    public Message updateMessage(@RequestParam("collection") String collectionName, @PathVariable String id, @RequestBody Message message) {
-        message.setId(id);
-        return messageService.updateMessage(collectionName, message);
-    }
-
-    @DeleteMapping("/{id}")
-    public void deleteMessage(@RequestParam("collection") String collectionName, @PathVariable String id) {
-        messageService.deleteMessage(collectionName, id);
-    }
-
-    @DeleteMapping
-    public void deleteAllMessages(@RequestParam("collection") String collectionName) {
-        messageService.deleteAllMessages(collectionName);
+    @GetMapping("/groups/{groupId}")
+    public ResponseEntity<List<MessageDTO>> getGroupChatMessages(@AuthenticationPrincipal MyUserDetails userDetails, @PathVariable String groupId){
+        List<MessageDTO> messageDTOS = messageService.getGroupChatMessages(userDetails, chatService.findById(new ObjectId(groupId)));
+        return ResponseEntity.ok(messageDTOS);
     }
 }
---
+++ src/main/java/com/example/chatsystem/dto/MessageDTO.java
@@ -0,0 +1,20 @@
+package com.example.chatsystem.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class MessageDTO {
+    private LocalDateTime timestamp;
+
+    private String message;
+
+    private String senderName;
+}
