1d473eafe29bcffe28c4e5ffc50112a7f50a4a55
Mahkamjon Raupov <devbrovki@gmail.com>
2024-06-16 01:10:03 +0200

    first commit

---
+++ src/main/java/com/example/chatsystem/ChatsystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chatsystem/ChatsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
