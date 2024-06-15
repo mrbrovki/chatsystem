df65729d4546deb653e194ee1be189a6668cd783
Mahkamjon Raupov <devbrovki@gmail.com>
2024-06-16 01:10:04 +0200

    chat dto update

---
+++ src/main/java/com/example/chatsystem/dto/ChatResponseDTO.java
@@ -0,0 +1,18 @@
+package com.example.chatsystem.dto;
+
+import com.example.chatsystem.model.MessageType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatResponseDTO {
+    String name;
+    String hostId;
+    String id;
+    MessageType type;
+}
