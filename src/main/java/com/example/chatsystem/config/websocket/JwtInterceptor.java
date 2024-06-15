46c5c0fb55b74eb7aa240d0b764b7933417cd580
Mahkamjon Raupov <devbrovki@gmail.com>
2024-06-16 01:10:04 +0200

    jwt interceptor works for subscription and sending

--- src/main/java/com/example/chatsystem/config/websocket/JwtInterceptor.java
+++ src/main/java/com/example/chatsystem/config/websocket/JwtInterceptor.java
@@ -1,51 +1,81 @@
 package com.example.chatsystem.config.websocket;
 
 import com.example.chatsystem.security.JwtService;
+import com.example.chatsystem.security.MyUserDetails;
+import com.example.chatsystem.security.MyUserDetailsService;
+import com.example.chatsystem.service.UserService;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 
 @Component
 public class JwtInterceptor implements ChannelInterceptor {
 
     private final JwtService jwtService;
-    private final UserDetailsService userDetailsService;
+    private final MyUserDetailsService userDetailsService;
+    private final UserService userService;
 
     @Autowired
-    public JwtInterceptor(JwtService jwtService, UserDetailsService userDetailsService) {
+    public JwtInterceptor(JwtService jwtService, MyUserDetailsService userDetailsService, UserService userService) {
         this.jwtService = jwtService;
         this.userDetailsService = userDetailsService;
+        this.userService = userService;
     }
 
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
         StompHeaderAccessor accessor =
                 MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
-        if (StompCommand.CONNECT.equals(accessor.getCommand())) {
-            System.out.println(accessor);
+        if (StompCommand.SEND.equals(accessor.getCommand())) {
             String authHeader = accessor.getFirstNativeHeader("Authorization");
-            String token = null;
-            String username = null;
+            String token = "";
             if(authHeader != null && authHeader.startsWith("Bearer ")){
                 token = authHeader.substring(7);
-                username = jwtService.extractUsername(token);
             }
-            if(username != null) {
-                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+            ObjectId userId = new ObjectId(jwtService.extractUserId(token));
+
+            if(!userId.toHexString().isEmpty()) {
+                MyUserDetails userDetails = userDetailsService.loadUserByUserId(userId);
                 UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
                 if(jwtService.validateToken(token, userDetails)) {
                     accessor.setUser(authenticationToken);
-                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                }
+            }
+        }
+
+
+        if (StompCommand.SUBSCRIBE.equals(accessor.getCommand())) {
+            String authHeader = accessor.getFirstNativeHeader("Authorization");
+            String token = "";
+            if(authHeader != null && authHeader.startsWith("Bearer ")){
+                token = authHeader.substring(7);
+            }
+            ObjectId userId = new ObjectId(jwtService.extractUserId(token));
+
+            String[] parts = accessor.getDestination().split("/");
+            if (parts[1].equals("user")) {
+                String destinationUsername = parts[2];
+                MyUserDetails userDetails = userDetailsService.loadUserByUserId(userId);
+
+                if (!userDetails.getUsername().equals(destinationUsername)) {
+                    throw new IllegalStateException("User is not authenticated for subscription");
+                }
+            } else {
+                String destinationChatId = parts[2];
+                List<ObjectId> userGroupChatIds = userService.findById(userId).getGroupChats();
+                if (!userGroupChatIds.contains(new ObjectId(destinationChatId))) {
+                    throw new IllegalStateException("User is not in the group for subscription");
                 }
             }
         }
--- src/main/java/com/example/chatsystem/config/websocket/WebSocketConfig.java
+++ src/main/java/com/example/chatsystem/config/websocket/WebSocketConfig.java
@@ -1,7 +1,10 @@
 package com.example.chatsystem.config.websocket;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -10,15 +13,30 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    private JwtInterceptor jwtInterceptor;
+    private CorsConfiguration corsConfiguration;
+
+    @Autowired
+    public void setJwtInterceptor(JwtInterceptor jwtInterceptor, CorsConfiguration corsConfiguration) {
+        this.jwtInterceptor = jwtInterceptor;
+        this.corsConfiguration = corsConfiguration;
+    }
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").withSockJS();
+        System.out.println(corsConfiguration.getAllowedOrigins());
+        registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:5173").withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("/user", "/topic");
+        registry.enableSimpleBroker("/user", "/group");
         registry.setUserDestinationPrefix("/user");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(jwtInterceptor);
     }
 }
