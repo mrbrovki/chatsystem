fe9eecd74c290a51e481cc9eb1d14733df7355db
Mahkamjon Raupov <devbrovki@gmail.com>
2024-08-06 23:53:51 +0200

    edit user endpoint

--- src/main/java/com/example/chatsystem/controller/UserController.java
+++ src/main/java/com/example/chatsystem/controller/UserController.java
@@ -1,12 +1,15 @@
 package com.example.chatsystem.controller;
 
+import com.example.chatsystem.dto.EditUserDTO;
 import com.example.chatsystem.model.User;
+import com.example.chatsystem.security.MyUserDetails;
 import com.example.chatsystem.service.UserService;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,10 +18,12 @@ import java.util.List;
 @RequestMapping("/api/v2/users")
 public class UserController {
     private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, PasswordEncoder passwordEncoder) {
         this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @GetMapping
@@ -55,5 +60,14 @@ public class UserController {
         } else {
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity<User> updateUser(@AuthenticationPrincipal MyUserDetails userDetails, @RequestBody EditUserDTO editUserDTO){
+        User user = userService.findById(new ObjectId(userDetails.getUserId()));
+        user.setUsername(editUserDTO.getUsername());
+        user.setHashedPassword(passwordEncoder.encode(editUserDTO.getPassword()));
+        userService.create(user);
+        return ResponseEntity.ok(user);
     }
 }
