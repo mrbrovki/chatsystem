6c9d0e5e9d26b1dee37d38f02c4bea0357b61a63
Mahkamjon Raupov <devbrovki@gmail.com>
2024-10-31 22:03:43 +0100

    cookie fix

--- src/main/java/com/example/chatsystem/utils/CookieUtils.java
+++ src/main/java/com/example/chatsystem/utils/CookieUtils.java
@@ -6,7 +6,7 @@ import jakarta.servlet.http.HttpServletResponse;
 public class CookieUtils {
     public static void addCookie(HttpServletResponse response, String name, String value, int maxAge, String domain) {
         Cookie cookie = new Cookie(name, value);
-        cookie.setPath("/api/");
+        cookie.setPath("/");
         cookie.setMaxAge(maxAge);
         cookie.setDomain(domain);
         cookie.setSecure(true);
